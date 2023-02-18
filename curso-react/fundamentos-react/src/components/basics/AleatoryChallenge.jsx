880a982a21148f24288784e432fb52bbad9ec407
João Marangoni <joao.tadeuzi00@gmail.com>
2023-02-18 15:22:13 -0300

    using destructuring

--- curso-react/fundamentos-react/src/components/basics/AleatoryChallenge.jsx
+++ curso-react/fundamentos-react/src/components/basics/AleatoryChallenge.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default props => {
-   const valueDrawn = Math.floor(Math.random() * (props.maxValue - props.minValue + 1) + props.minValue);
+   const {min, max} = props;
+
+   const valueDrawn = Math.floor(Math.random() * (max - min + 1) + min);
    return (
       <div>
-      <h1>Look for average value between: { props.minValue } and { props.maxValue }</h1>
-      <h2>Value drawn: { valueDrawn }</h2>
-   </div>
+         <h1>Look for average value between: <strong>{ min }</strong> and <strong>{ max }</strong></h1>
+         <h2>Value drawn: { valueDrawn }</h2>
+      </div>
    ) 
 }
